6e24d73dbf32b093f1d75ecb59e1f471ec1250d2
Codatrix <ankur.codatrix@gmail.com>
2022-08-10 09:47:15 +0530

    Conditionals in Python

---
+++ 02-Python-Basics-II/01-Conditional-Logic.py
@@ -0,0 +1,22 @@
+is_old_enough = False
+has_license = True
+
+
+if is_old_enough:
+    print("You are old enough")
+elif has_license:
+    print("You have a license")
+else:
+    print("You are not of age!!")
+
+print("OkOk")
+
+# -------------------------------------------
+
+is_old_enough = True
+has_license = True
+
+if is_old_enough and has_license:
+    print("You can drive the car")
+else:
+    print("Sorry! you can't drive")
