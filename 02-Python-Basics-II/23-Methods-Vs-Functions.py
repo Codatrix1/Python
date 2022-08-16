43e23d58b1a819f2e35e98cfd6bddd4af6bc75fe
Codatrix <ankur.codatrix@gmail.com>
2022-08-16 10:30:37 +0530

    Methods Vs Functions - Docstrings - Magic Method __doc__

---
+++ 02-Python-Basics-II/23-Methods-Vs-Functions.py
@@ -0,0 +1,21 @@
+# ----------------------
+# Methods VS Functions
+# -----------------------
+
+# Functions
+
+len()
+min()
+max()
+print()
+input()
+
+
+def some_random_function():
+    pass
+
+
+# Methods
+
+"hellooo".upper()
+"hellooooo".count("o")
---
+++ 02-Python-Basics-II/24-Docstrings.py
@@ -0,0 +1,12 @@
+# Function
+def some_function(a):
+    """
+    Info: This function tests and prints param a
+    """
+    print(a)
+
+
+# help(some_function) # Information about the function
+
+# Magic Methods or Dunder methods
+print(some_function.__doc__)  # Provides the docstring content
